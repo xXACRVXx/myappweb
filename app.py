9f0b476e0ff70b29cef662d87b4d60ef4490aba6
xXACRVXx <70526454+xXACRVXx@users.noreply.github.com>
2022-03-01 13:26:00 -0600

    update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sqlite3
 
 
 app = Flask(__name__, template_folder=".")
-app.secret_key = 'myfukinsecretkey'
+app.secret_key = 'mysecretkey'
 @app.route('/')
 def index():
   return render_template('index.html')
